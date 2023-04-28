e888086a45b86653a45b496108e54d381aadc5ca
Gustavo <jose.gustavo.1989@gmail.com>
2023-04-28 16:32:22 -0700

    Creando repositorio para los codigos del encriptador

---
+++ utils/encrypt.js
@@ -0,0 +1,43 @@
+const keys = {
+  a: "ai",
+  e: "enter",
+  i: "imes",
+  o: "ober",
+  u: "ufat",
+}
+
+export const encrypt = text => {
+  
+  const letters = text.split("")
+
+  
+  const encrypted = letters
+    .map(l => {
+     
+      return keys[l] || l
+    })
+    .join("") 
+
+  
+  return encrypted
+}
+
+export const decrypt = text => {
+  
+  const keysArray = Object.entries(keys)
+
+ 
+  let result = text
+
+  
+  keysArray.forEach(([key, value]) => {
+    
+    const regex = new RegExp(value, "g")
+
+    
+    result = result.replace(regex, key)
+  })
+
+ 
+  return result
+}
